0abe14389a864f5b697e4fbefbfda4a7d73f56b2
VTR <vtr@planning-macbooks-MacBook-Air.local>
2017-10-24 11:51:29 +0200

    Reading command line input now Cleaned up code and added input reads, echoing instructions to the users and reading command line input to improve human usability for non-ffmpeg aficionados

--- 01_bash/99_ambassadors/basic_convert.sh
+++ 01_bash/99_ambassadors/basic_convert.sh
@@ -2,32 +2,37 @@
 
 IFS=$'\n'
 
-# f=$1
-
-if [ -z "$1" ]; then
+# if [ -z "$1" ]; then
 	echo "
-	First variable not set. Please enter input info in the following order:
-	1. Input file or folder
-	2. Video Codec (specify 'vn' if the file is audio only)
-	3. Video Bitrate
-	4. Audio Codec (specify 'an' is the file is video only)
- 	5. Audio Bitrate
-	6. Destination format
-	7. Destination folder
-	"
+	Please enter input info in the following order:
+	1. Input file or folder"
+	read 1
+	echo "2. Video Codec (specify 'vn' if the file is audio only)"
+	read 2
+	echo "3. Video Bitrate"
+	read 3
+	echo "4. Audio Codec (specify 'an' is the file is video only)"
+	read 4
+ 	echo "5. Audio Bitrate"
+	read 5
+	echo "6. Destination format"
+	read 6
+	echo "7. Destination folder"
+	read 7
 
-else
+# else
 
-	for $f in $(find $1 -type f -iname "*.mov" -o -iname "*.mp4" -o -iname "*.mxf" -o -iname "*.avi" -o -iname "*.wmv"); do
+	vf=$(find $inFolder -type f -iname "*.mov" -o -iname "*.mp4" -o -iname "*.mxf" -o -iname "*.avi" -o -iname "*.wmv")
+		for f in $vf; do
+		basef=$(basename "$f")
 
-	basef=$(basename "$f")
+			if [ \( "$2" == "vn" \) ] || [ \( -z "$2" \) ]; then
+				ffmpeg -i -c:a $4 -b:a $5 -f $6 $7/$basef-audio.$6
+			elif  [ \( "$4" == "an" \) ] || [ \( -z "$4" \) ]; then
+				ffmpeg -i $f -c:v $2 -b:v $3 -f $6 $7/$basef-mute.$6
+			else
+				ffmpeg -i $f -c:v $2 -b:v $3 -c:a $4 -b:a $5 -f $6 $7/$basef-audio.$6
+			fi
 
-		if [ \( "$2" -eq vn \) -o \( -z "$2" \) ]; do
-			ffmpeg -i -c:a $4 -b:a $5 -f $6 $7/$basef-audio.$6
-		elif  [ \( "$4" -eq an \) -o \( -z "$4" \) w]; do
-			ffmpeg -i $f -c:v $2 -b:v $3 -f $6 $7/$basef-mute.$6
-		else
-			ffmpeg -i $f -c:v $2 -b:v $3 -c:a $4 -b:a $5 -f $6 $7/$basef-audio.$6
-		fi
-
-	done
+		done
+# fi
