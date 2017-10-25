edfdf5ff42d15127d8510dde05d9df67e71a470d
VTR <vtr@planning-macbooks-MacBook-Air.local>
2017-10-25 15:28:01 +0200

    Fixing broken code cause I'm a dweeb cause it wouldn't work the way i re-wrote it, because the mp4 was dependant on the master being present

--- 01_bash/99_ambassadors/relay.sh
+++ 01_bash/99_ambassadors/relay.sh
@@ -22,15 +22,21 @@ if [ -z "${1+x}" ]; then
 	The output will be placed in the destination folder as both a Master (.mov) and Preview (.mp4) file.
 	"
 
-elif [[ "$3" != *mov ]] || [[ "$3" != master ]]; then
+elif [[ "$3" == mov ]] || [[ "$3" == master ]]; then
+	echo "Relaying audio to video as a Master file..."
 	mkdir -p $4
-		ffmpeg -hide_banner -y -i "$4/$basef--$basea--$date.mov" \
+		ffmpeg -hide_banner -loglevel panic -y -i "$f" -i "$a" \
+			-c:v copy -map 0:0 -map 1:0 -c:a copy \
+			"$4/$basef-$date.mov"
+	echo "...Done! File created at $4/$basef-$date.mov"
+
+elif [[ "$3" != mov ]] || [[ "$3" != master ]]; then
+	echo "Creating mp4 file..."
+	mkdir -p $4
+		ffmpeg -hide_banner -loglevel panic -y -i "$f" -i "$a" \
+			-c:v copy -map 0:0 -map 1:0 -c:a copy \
 			-c:v libx264 -c:a aac -b:v "$3"k -b:a 256k \
 			-profile:v high -level 41 -pix_fmt yuv420p \
 			-f mp4 "$4/$basef--$basea--$date-$time.mp4"
-elif [[ "$3" == *mov ]] || [[ "$3" == master ]]; then
-	mkdir -p $4
-		ffmpeg -hide_banner -i "$f" -i "$a" \
-		-c:v copy -map 0:0 -map 1:0 -c:a copy \
-		"$4/$basef--$basea--$date.mov"
+	echo "...Done! File created at $4/$basef--$basea--$date-$time.mp4"
 fi
