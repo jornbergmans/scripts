46c0f0458813fee2621a167271ae480f7ac7a303
VTR <vtr@planning-macbooks-MacBook-Air.local>
2017-10-25 14:43:35 +0200

    Updates to basic relay script Updated basic convert for cleaner code, update to Relay script to only relay to mp4 unless the output is specified as mov or master

--- 01_bash/99_ambassadors/relay.sh
+++ 01_bash/99_ambassadors/relay.sh
@@ -22,9 +22,15 @@ if [ -z "${1+x}" ]; then
 	The output will be placed in the destination folder as both a Master (.mov) and Preview (.mp4) file.
 	"
 
-else
-
-mkdir -p $4 && ffmpeg -hide_banner -i $f -i $a -c:v copy -map 0:0 -map 1:0 -c:a copy $4/$basef--$basea--$date.mov
-ffmpeg -hide_banner -y -i "$4/$basef--$basea--$date.mov" -c:v libx264 -c:a aac -b:v "$3"k -b:a 256k -profile:v high -level 41 -pix_fmt yuv420p -f mp4 "$4/$basef--$basea--$date-$time.mp4"
-
+elif [ "$3" != mov ]; then
+	mkdir -p $4
+		ffmpeg -hide_banner -y -i "$4/$basef--$basea--$date.mov" \
+			-c:v libx264 -c:a aac -b:v "$3"k -b:a 256k \
+			-profile:v high -level 41 -pix_fmt yuv420p \
+			-f mp4 "$4/$basef--$basea--$date-$time.mp4"
+elif [ "$3" == mov ]; then
+	mkdir -p $4
+		ffmpeg -hide_banner -i "$f" -i "$a" \
+		-c:v copy -map 0:0 -map 1:0 -c:a copy \
+		"$4/$basef--$basea--$date.mov"
 fi
