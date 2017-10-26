6181991ce5ec4aac76b975a25e19c47013461448
VTR <vtr@planning-macbooks-MacBook-Air.local>
2017-10-26 13:02:20 +0200

    Updated relay script in Python with check Now checks for audio and video length and only does a relay when they are identical. Also, updated the script to add steps to the ffmpeg command, like in seqdirs. Input still all given as command line arguments. Dont forget the k in video bitrate! (Or your video will look especially shitty)

--- 02_python/rl.py
+++ 02_python/rl.py
@@ -21,60 +21,104 @@ outfolder = sys.argv[4]
 basev = os.path.basename(vin)
 now = datetime.datetime.now().strftime('%Y%m%d-%H%M')
 
-vin_arguments = [
+probe_header = [
     '/usr/local/bin/ffprobe',
     '-v', 'error',
     '-hide_banner',
+]
+probe_arguments = [
     '-show_entries', 'format=duration',
     '-of', 'default=noprint_wrappers=1:nokey=1',
-    '-i', vin
 ]
 
-ain_arguments = [
-    '/usr/local/bin/ffprobe',
-    '-v', 'error',
-    '-hide_banner',
-    '-show_entries', 'format=duration',
-    '-of', 'default=noprint_wrappers=1:nokey=1',
-    '-i', ain
-]
+if outformat == "mov":
+    outext = "mov"
+else:
+    outext = "mp4"
 
 outname = os.path.join(
     outfolder,
     "{}_{}.{}".format(
         os.path.splitext(basev)[0],
         now,
-        outformat
+        outext
     )
 )
 
-avmix = [
+ff_header = [
     '/usr/local/bin/ffmpeg', '-hide_banner',
+    '-loglevel', 'warning',
+    '-y',
     '-i', vin,
     '-i', ain,
+    '-map', '0:0', '-map', '1:0',
+]
+
+ff_master = [
     '-c:v', 'copy',
     '-c:a', 'copy',
-    '-map', '0:0', '-map', '1:0',
     '-f', 'mov',
     outname
 ]
 
-# return video and audio outputs as bytes, decode into float
-# then round the float to nearest integer to compare vin to ain
+ff_ref = [
+    '-c:v', 'libx264',
+    '-c:a', 'aac',
+    '-b:v', outformat,
+    '-b:a', '160k',
+    '-pix_fmt', 'yuv420p',
+    '-profile:v', 'high',
+    '-level', '41',
+    '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',
+    '-f', 'mp4',
+    outname
+]
 
-vout = sp.check_output(
-    vin_arguments
-)
-vint = round(float(vout.decode().strip()))
+if __name__ == "__main__":
 
-aout = sp.check_output(
-    ain_arguments
-)
-aint = round(float(aout.decode().strip()))
+    # return video and audio outputs as bytes, decode into float
+    # then round the float to nearest integer to compare vin to ain
+    vprobe = []
+    vprobe.extend(probe_header)
+    vprobe.extend(['-i', vin])
+    vprobe.extend(probe_arguments)
+    vout = sp.check_output(
+        vprobe
+    )
+    vint = vout.decode()
 
-if vint == aint:
-    sp.run(avmix)
-else:
-    print("Length of video and audio files do not match!")
-    print("Length of video file is " + str(vint) + " seconds")
-    print("Length of audio file is " + str(aint) + " seconds")
+    aprobe = []
+    aprobe.extend(probe_header)
+    aprobe.extend(['-i', ain])
+    aprobe.extend(probe_arguments)
+    aout = sp.check_output(
+        aprobe
+    )
+    aint = aout.decode()
+    # aint = float(aout.decode())
+
+    avmix = []
+    avmix.extend(ff_header)
+
+    if outformat == "mov":
+        avmix.extend(ff_master)
+    else:
+        avmix.extend(ff_ref)
+
+    # test prints
+    # print('audioint', aint)
+    # print('vidint', vint)
+    # print(avmix)
+
+    if vint == aint:
+        if outformat == "mov":
+            print("Creating Master file")
+            sp.run(avmix)
+        elif outformat != "mov":
+            print("Creating reference file")
+            sp.run(avmix)
+        print("Done! Created file at ", outname)
+    else:
+        print("Length of video and audio files do not match!")
+        print("Length of video file is " + str(vint) + " seconds")
+        print("Length of audio file is " + str(aint) + " seconds")
