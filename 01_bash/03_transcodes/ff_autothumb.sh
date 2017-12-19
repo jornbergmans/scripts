6ca7d2e3ea10e05b77d5dfe1986d552520451d6b
VTR <vtr@planning-macbooks-MacBook-Air.local>
2017-12-19 16:11:01 +0100

    Adding one-pass thumbnail creator

---
+++ 01_bash/03_transcodes/ff_autothumb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+echo "Please input file"
+	read inFile
+	inVid=$(echo "$inFile" | sed 's/^[ \t]*//;s/[ \t]*$//')
+if [[ -f ${inVid} ]]; then
+	echo "Thank You. Reading from file $inVid"
+
+	bdIn=$(dirname "$inVid")
+	baseIn=$(basename "$inVid")
+
+	inRate="20"
+	inFrames=$(		ffprobe -hide_banner -loglevel panic -pretty \
+								-select_streams v:0 -show_entries stream=nb_frames \
+								-of default=noprint_wrappers=1:nokey=1 -i $inVid)
+	fps=$(				ffprobe -hide_banner -loglevel panic -pretty \
+								-select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 -i $inVid)
+	outToGrab=$(echo "$inFrames/$inRate" | bc)
+	outFrames=$(echo "$outToGrab/$fps" | bc)
+	outRate=$(echo "$inFrames/$outFrames" | bc)
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo " "
+echo "I see that the video has $inFrames frames"
+echo "You have chosen to output 1 thumbnail image per $inRate seconds"
+echo "That equals a thumbnail per $outRate frames"
+echo "I will output a filmstrip that is $outFrames frames in width"
+echo " "
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+ffmpeg 	-hide_banner -loglevel panic \
+				-y -i $inVid -frames 1 -q 5 \
+				-vf "mpdecimate,select=not(mod(n\,$outRate)),scale='480:-1',tile='$outFrames'x1" \
+				 "$bdIn/$baseIn-thumb.png"
+
+	echo " "
+	echo "Thumbnail output file created at $bdIn/$baseIn-thumb.png"
+	
+else
+	echo "$inVid is not a valid file. Please restart and input a valid source video file."
+	exit 1
+fi
