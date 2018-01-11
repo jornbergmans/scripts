49ccabdb215ec1f733e33d815e97367979548f3b
VTR02 <VTR02@vtr02.vfx.theambassadors.nl>
2018-01-11 14:48:42 +0100

    Updated md5 script to search only the destination directory for the filename of the original input file. This is to prevent two files with the same name throwing hash errors because they don't have the same md5 as eachother, because these files originate from a completely different folder. For example, Camera Card 1 - File 001.mov and Camera Card 2 - File 001.mov will both be read as 001.mov but will not have the same md5. Script was changed to only look for 001.mov in "Camera Card 1" folder if the original file was from "Camera Card 1".

--- 01_bash/02_moves/sync-and-md5.sh
+++ 01_bash/02_moves/sync-and-md5.sh
@@ -9,7 +9,7 @@ if [[ -z "$1" ]]; then
 fi
 
 if [[ ! "$#" -ge 2 ]]; then
-  echo "Please supply both input folder and output folder(s),"
+  echo "Please provide both input folder and output folder(s),"
   echo "starting with the input folder as source of the original files,"
   echo "then output folder(s) as destination(s) for copies."
   exit 1
@@ -31,26 +31,55 @@ elif [[ "$#" -ge 2 ]]; then
   fi
     base3=$(basename "$3")
 
-  ## Start the data transfer using rsync.
-  ## Let us know what you're doing, and log it
-  rsync -ahv --log-file="$2/$base2-$datetime.log" "$inFolder" "$2"
-  rsync -ahv --log-file="$3/$base3-$datetime.log" "$inFolder" "$3"
+  ## After creating the folder, start the data transfer using rsync.
+  ## Let us know what you're doing, and log it.
+    rsync -ahv --log-file="$2/$base2-$datetime.log" "$inFolder" "$2"
+  if [[ -n "$3" ]] || [[ -d "$3" ]]; then
+    rsync -ahv --log-file="$3/$base3-$datetime.log" "$inFolder" "$3"
+  fi
 
   ## Check all files for md5 checksum matches
   ## make a list of all the files
-  inputFileList=$(find $1 -type f)
+  inputFileList=$(find $inFolder -type f -and -not -iname ".*")
 
   ## loop over this list
   for inFile in $inputFileList; do
     inFileBase=$(basename "$inFile")
+    inFileFolder=$(dirname "$inFile")
+    inFileSearchFolder=$(echo $inFileFolder | sed 's:^.*\/*\/::g')
 
   ## make an md5 hash for the originals
     md5In=$(md5 -q "$inFile")
 
   ## search copied files 1 for the same filename
   ## and make an md5 hash
-    out1File=$(find "$2" -type f -iname "$inFileBase")
+    out1File=$(find "$2" -type f -and -iname "$inFileBase" -and -ipath "*/$inFileSearchFolder/*")
     md5Out1=$(md5 -q "$out1File")
+    out1FileBase=$(basename $out1File)
+
+      mkdir -p "$2/md5"
+      echo "" >> "$2/md5/copyreport.log"
+      echo $inFileBase >> "$2/md5/copyreport.log"
+      echo "Input file location" - $inFile >> "$2/md5/copyreport.log"
+      echo "Input file hash - MD5 =" $md5In >> "$2/md5/copyreport.log"
+      echo "Copied file location" - $out1File >> "$2/md5/copyreport.log"
+      echo "Copied file hash - MD5 =" $md5Out1 >> "$2/md5/copyreport.log"
+      echo "" >> "$2/md5/copyreport.log"
+
+#echo "++++++++++"
+#echo "InFile"
+#echo $inFile
+#echo "++++++++++"
+#echo "inFileBase"
+#echo $inFileBase
+#echo "++++++++++"
+#echo "inFolderBase"
+#echo $inFileSearchFolder
+#echo "++++++++++"
+#echo "outFile"
+#echo $out1File
+#echo "++++++++++"
+
   ## compare these hashes, tell us when something is wrong and log it to a file
     if [[ "$md5In" != "$md5Out1" ]]; then
       echo "Hash mismatch! md5 checksum for file $inFile"
@@ -65,13 +94,23 @@ elif [[ "$#" -ge 2 ]]; then
 
   ## do the same thing for the backup copy if the directory was supplied
     if [[ -n "$3" ]] || [[ -d "$3" ]]; then
-      out2File=$(find "$3" -type f -iname "$inFileBase")
+      out2File=$(find "$3" -type f -and -iname "$inFileBase" -and -ipath "*/$inFolderBase/")
       md5Out2=$(md5 -q "$out2File")
+      out2FileBase=$(basename $out2File)
+
+      mkdir -p "$3/md5"
+      echo "" >> "$3/md5/copyreport.log"
+      echo $inFileBase >> "$3/md5/copyreport.log"
+      echo "Input file location" - $inFile >> "$3/md5/copyreport.log"
+      echo "Input file hash - MD5 =" $md5In >> "$3/md5/copyreport.log"
+      echo "Copied file location" - $out1File >> "$3/md5/copyreport.log"
+      echo "Copied file hash - MD5 =" $md5Out1 >> "$3/md5/copyreport.log"
+      echo "" >> "$3/md5/copyreport.log"
+
       if [[ "$md5In" != "$md5Out2" ]]; then
         echo "Hash mismatch! md5 checksum for file $inFile"
         echo "does not match checksum for $out2File"
         echo "Please varify files manually. This error will be logged."
-        mkdir -p "$3/md5"
         echo $inFileBase >> "$3/md5/mismatch.log"
         echo "Input file hash - MD5" $inFile "=" $md5In >> "$3/md5/mismatch.log"
         echo "Copied file hash - MD5" $out2File "=" $md5Out2 >> "$3/md5/mismatch.log"
