4c2130e979678c556a4eb0a0dbc432aaae8a52ad
VTR02 <VTR02@vtr02.vfx.theambassadors.nl>
2018-01-17 15:14:03 +0100

    small adjustments to verbosity

--- 01_bash/99_ambassadors/ref-finals.sh
+++ 01_bash/99_ambassadors/ref-finals.sh
@@ -19,19 +19,18 @@ fi
 
 # function refs {
 
-if [[ "$1" = */archived/* ]]; then
+if [[ "$1" = */*_archive/* ]]; then
   echo "Archive check passed"
 
 
   if [ -d "$1" ]; then
     echo "Folder check passed"
-
-			for fin in 	$(find "$1" -depth -type f -iname "*.mov" -or -iname "*.mxf" -and -not -iname "._*" ); do
-
+	fin_list=$(find "$1" -depth -type f -and -iname "*.mov" -or -iname "*.mxf" -and -not -iname "._*" )
+			for fin in $fin_list; do
 
 				if [ "$debug" = false ] ; then
-
-					ffmpeg -y -i "$fin" -c:v libx264 -b:v 1500k -c:a aac -strict 2 -b:a 96k \
+					echo "Creating reference file for $fin"
+					ffmpeg -hide_banner -loglevel error -y -i "$fin" -c:v libx264 -b:v 1500k -c:a aac -strict 2 -b:a 96k \
 					-pix_fmt yuv420p -profile:v main -level 3.1 -f mp4 $fin-ref.mp4 ;
 
 					rm -f "$fin"
