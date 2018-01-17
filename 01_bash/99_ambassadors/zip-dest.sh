b4876a8feeb8f5b50da5e4c1f0e9a169bcaca20e
VTR <vtr@planning-macbooks-MacBook-Air.local>
2018-01-17 15:26:00 +0100

    Moved single file in 00_ folder to 99_ folder

--- 01_bash/99_ambassadors/zip-dest.sh
+++ 01_bash/99_ambassadors/zip-dest.sh
@@ -4,6 +4,7 @@ IFS=$'\n'
 # # #
 
 zipdir=$1
+
 dest=$2
 
 debug=$4
@@ -13,66 +14,50 @@ else
 	debug=debug
 fi
 
-if [[ $debug = debug ]] ; then
+if [ $debug = debug ] ; then
   echo "Debug mode on. Checking for folders to zip, please wait..."
-# elif [[ $debug = "true|false|live" ]] ; then
-# 	echo "Running script in live mode, please wait..."
 fi
 
+# # #
 for zipfolder in $(find $zipdir -depth -type d); do
 
-filelist=$(find $zipfolder -mindepth 1 -maxdepth 1 -type f -and -iname "*$3*" -and -not -iname ".*" -and -not -iname "*.zip")
-count=$(find $zipfolder -mindepth 1 -maxdepth 1 -type f -and -iname "*$3*" -and -not -iname ".*" -and -not -iname "*.zip" | wc -l)
+filelist=$(find $zipfolder -mindepth 1 -maxdepth 1 -type f -size -80M -and -iname "*$3*" -and -not -iname ".*" -and -not -iname "*.zip")
+count=$(find $zipfolder -mindepth 1 -maxdepth 1 -type f -size -80M -not -iname ".*" -and -not -iname "*.zip" | wc -l)
 
 basedest=$(basename "$zipfolder")
 dirdest=$(dirname "$zipfolder")
 zipname=$(basename "$dirdest")
 #basedest=$(echo $zipfolder | sed 's:/Volumes/::')
 
-#   	if [[ $count -gt 20 ]]; then
 
-			# echo "basedest = $basedest"
-			# echo "dirdest = $dirdest"
-			# echo "zipname = $zipname"
-			# echo "full name will be $dest/$zipname/$zipname-$basedest.zip"
-			# fi
-
-   	  if [[ $debug = debug ]] ; then
-        echo " "
-        echo "I will archive $count files in $zipfolder that contain the string \"$3\""
-    	  else
-        	if [[ -f $dest/$zipname/$zipname-$basedest.zip ]]
-				then
-		   		echo "Zip file $dest/$zipname/$zipname-$basedest.zip already exists, skipping."
-      		echo " "
-				else
-      			mkdir -p $dest/$zipname/
-        		echo "Zipping $count files in $zipfolder, please wait..."
-#        		for files in $filelist; do
-	        		zip -jq1 $dest/$zipname/$zipname-$basedest.zip $filelist \;
-#	    		done
-				#depricated	zip -r0 	  $zipfolder/$basezips.zip $filelist
-       			echo "Zip of $zipfolder completed!"
-						echo "The file can be found at $dest/$zipname/$zipname-$basedest.zip"
-						echo " "
-       	  	fi
-							if [[ $3 = png ]] || [[ $3 = dpx ]] || [[ $3 = exr ]] || [[ $3 = tif* ]] || [[ $3 = jp*g ]]; then
-					      if [[ ! -f $dest/$zipname/$zipname-$basedest.mp4 ]]
-									then
-										echo "A reference file will be created at $dest/$zipname/$zipname-$basedest.mp4"
-										ffmpeg -hide_banner -loglevel panic -pattern_type glob \
-										-y -i "$zipfolder/*.$3" -c:v libx264 -b:v 2500k \
-										-pix_fmt yuv420p -r 25 -f mp4 "$dest/$zipname/$zipname-$basedest.mp4"
-										echo " "
-								fi
-							# else
-							# 	echo "Files are not images, no reference video will be created for zipfile."
-						 fi
-       # echo "Truncating files"
-       # $filelist -delete
-   	  fi
-
-#    fi
+echo "basedest = $basedest"
+echo "dirname = $dirname"
+echo "zipname = $zipname"
+echo "full name will be $dest/$zipname/$zipname-$basedest.zip"
+#    	if [[ $count -gt 20 ]]; then
+#
+#    	  if [ $debug = debug ] ; then
+#         echo " "
+#         echo "I will zip $zipfolder because it has $count files in it"
+#     	  else
+#         	if [ -e $dest/$zipname/$zipname-$basedest.zip ]
+# 				then
+# 		   		echo "Zip file $dest/$zipname/$zipname-$basedest.zip already exists, skipping."
+#       		else
+#       			mkdir -p $dest/$dirdest/
+#         		echo "Zipping $count files in $zipfolder, please wait..."
+# #        		for files in $filelist; do
+# 	        		zip -jq1 $dest/$zipname/$zipname-$basedest.zip $filelist \;
+# #	    		done
+# 				#depricated	zip -r0 	  $zipfolder/$basezips.zip $filelist
+#        			echo "Zip of $zipfolder completed!"
+# 						echo "The file can be found at $dest/$zipname/$zipname-$basedest.zip"
+#        	  	fi
+#        # echo "Truncating files"
+#        # $filelist -delete
+#    	  fi
+#
+#     fi
 
 done
 
@@ -88,10 +73,3 @@ echo "Nothing else to do!"
 #         #
 #         # echo $filelist
 #         echo ${#filelist[@]}
-
-
-## Make a video from an image sequence with 4 trailing digits:
-## ffmpeg -y -i /foo/bar_%04d.png -c:v libx264 -b:v 2500k -pix_fmt yuv420p -r 25 -f mp4 /foo/bar.mp4
-
-## or with no trailing digits or regular pattern:
-## ffmpeg -y -i /foo/bar*.png -c:v libx264 -b:v 2500k -pix_fmt yuv420p -r 25 -f mp4 /foo/bar.mp4
