20e771a38a8ce722b9c37039585b74ce2e5b2e25
VTR02 <VTR02@vtr02.vfx.theambassadors.nl>
2018-03-16 13:55:09 +0100

    removed excess newlines

--- 01_bash/01_finds/du_active.sh
+++ 01_bash/01_finds/du_active.sh
@@ -8,20 +8,17 @@ datetime=$(date +%Y%m%d-%H%M)
 # # # # # # # # # # # #
 
 # echo "DISK USAGE ON /AMBASSADORS_SHARED, FOLDER PATH" > /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/hammer_$datetime.csv
-du -kd1 /AMBASSADORS_SHARED | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/shared/shared_$datetime.csv
+du -kd1 /AMBASSADORS_SHARED | sort -nr | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/shared/shared_$datetime.csv
 # echo " "
 # echo "DISK USAGE ON /HAMMER, FOLDER PATH" >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/hammer_$datetime.csv
-du -kd1 /HAMMER | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' \
-| sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/hammer/hammer_$datetime.csv
+du -kd1 /HAMMER | sed $'s/[[:blank:]]/,/;s/\/.*\///;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/hammer/hammer/hammer_$datetime.csv
 # | sed $'s/[[:blank:]]/,/;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' \
 
 # # # # # # # # # # # #
 #    The Projects     #
 # # # # # # # # # # # #
 
-find /AMBASSADORS_SHARED/PROJECTS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; \
-  | sed 's/\(\,[[:blank:]]\]\,\)/\]\,/g;s/[[:space:]]/-/g;s/\///g;/\,\./d' \
-  | sort -nr  >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/projs/projdirs_$datetime.csv
+find /AMBASSADORS_SHARED/PROJECTS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed 's/\(\,[[:blank:]]\]\,\)/\]\,/g;s/[[:space:]]/-/g;s/\///g;/\,\./d' | sort -nr  >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/projs/projdirs_$datetime.csv
   # | sed 's/[[:space:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:blank:]]/-/g' \
 
 
@@ -30,15 +27,11 @@ find /AMBASSADORS_SHARED/PROJECTS/ -type d -and -iname "*_p1??????" -and -not -i
 # # # # # # # # # # # #
 
 # echo "DISK USAGE ON ODIN, FOLDER PATH" > /AMBASSADORS_SHARED/USERS/Jorn/servsizes/odin/odin_$datetime.csv
-# du -kd1 /ODIN | sed $'s/[[:blank:]]/,/;s/\/.*\///' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/odin/odin_$datetime.csv
-
-find /ODIN/_WORK/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; \
-  | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' \
-  | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/work/workdirs_$datetime.csv
-
-find /ODIN/LIBRARY/pFINALS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; \
-  | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' \
-  | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/fins/pfins_$datetime.csv
+du -kd1 /ODIN | sed $'s/[[:blank:]]/,/;s/\/.*\///' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/odin/odin_$datetime.csv
+#
+find /ODIN/_WORK/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/work/workdirs_$datetime.csv
+#
+find /ODIN/LIBRARY/pFINALS/ -type d -and -iname "*_p1??????" -and -not -ipath "*_p1??????/*_p1??????*" -exec du -kd0 {} \; | sed 's/[[:blank:]]/,/g;s/\/.*\///' | sed '/\(\..*\)/d;s/\///g;s/[[:space:]]/-/g' | sort -nr >> /AMBASSADORS_SHARED/USERS/Jorn/servsizes/fins/pfins_$datetime.csv
 
 # # # # # # # # # # # #
 #    OBSOLETE CODE    #
