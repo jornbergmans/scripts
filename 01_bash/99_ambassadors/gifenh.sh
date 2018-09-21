89ffd267d3d9fc550cbcdefc93232625b3e5c706
VTR02 <VTR02@vtr02.vfx.theambassadors.nl>
2018-09-21 14:32:28 +0200

    Update gif creator

--- 01_bash/99_ambassadors/gifenh.sh
+++ 01_bash/99_ambassadors/gifenh.sh
@@ -1,33 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 IFS=$'\n'
 
+
+# 	echo "
+# 	First variable not set. Please enter input info in the following order:
+# 	1. Input file or folder
+# 	2. Output framerate
+# 	3. Output height in pixels
+# 	4. Debug mode enables
+# 	"
+#
+# else
 if [ -z "$1" ]; then
-	echo "
-	First variable not set. Please enter input info in the following order:
-	1. Input file or folder
-	2. Output framerate
-	3. Output height in pixels
-	"
-
+	echo "Please input folder"
+	read inputfolder
+	echo "Please input desired output framerate"
+	read outputrate
+	echo "Please input desired output height in pixels"
+	read rez
+	echo "Do you want to enable debug mode? y/n"
+	read debug
 else
+	inputfolder="$1"
+	outputrate="$2"
+	rez="$3"
+	debug="$4"
+fi
+filelist=$(find "$inputfolder" -iname "*GIF*.mov" -not -iname "._*")
 
-mkdir -p $1/gif-$3
-
-for f in $(find $1 -iname "*GIF*.mov" -not -iname "._*"); do
-	basef=$(basename $f)
-		ffmpeg -y -i $f -vf fps=10,scale=-1:1080:flags=lanczos,palettegen $1/gif-$3/$basef-palette.png
-
-		ffmpeg -y -i $f -i $1/gif-$3/$basef-palette.png -filter_complex \
-		"fps=$2,scale=-1:$3:flags=lanczos[x];[x][1:v]paletteuse" -f gif $1/gif-$3/$basef.gif
+ for f in $filelist; do
+ 	 basef=$(basename "$f")
+	 dirf=$(dirname "$f")
+	 outputdir="$dirf"/gif-"$rez"p"$outputrate"
+	 mkdir -p "$outputdir"
+		if [[ ! "$debug" = '' ]] && [[ "$debug" = 'y' ]]; then
+		 echo "- - - -"
+		 echo "Debug mode enabled, listing input / output"
+		 echo "- - - -"
+		 echo "f is $f"
+		 echo "outputdir is $outputdir"
+		 echo "dirf is $dirf"
+		 echo "basef is $basef"
+		 echo "- - - -"
+ 		else
+		 echo "Creating palette for $basef"
+				ffmpeg -hide_banner -loglevel panic -y -i "$f" -vf fps=10,scale=-1:1080:flags=lanczos,palettegen "$outputdir/${basef/.mov/_palette.png}"
+		 echo "Creating GIF file at $2 frames per second"
+				ffmpeg -hide_banner -loglevel panic -y -i "$f" -i "$outputdir"/"${basef/.mov/_palette.png}" -filter_complex \
+				"fps=$outputrate,scale=-1:$rez:flags=lanczos[x];[x][1:v]paletteuse" -f gif "$outputdir/${basef/.mov/_"$rez"p"$outputrate".gif}"
+		 echo "GIF file created at $outputdir/${basef/.mov/_"$rez"p"$outputrate".gif}"
+		 echo ""
+	 	fi
 done
 
-for name in $(find $1/gif-$3/ -iname "*mov.gif"); do
-		mv "${name}" ${name/mov.gif/gif}
-done
+#for name in $(find $1/gif-$3/ -iname "*mov.gif"); do
+#		mv "${name}" ${name/mov.gif/gif}
+#done
 #
 # for gif in $(find $1 -type f -and -iname "*.gif" -or -iname "*palette*.png"); do
 # 		mv "$gif" $1/gif-$3/
 # done
-
-fi
+#
+# fi
