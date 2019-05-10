27fb87d8214687453dc343ad10ea6a4ab36d5891
Jorn Bergmans <jorn.bergmans@ambassadors.studio>
2019-05-10 10:42:06 +0200

    small tweaks, as always

--- 01_bash/03_transcodes/mov-cut-sec-rewrap.sh
+++ 01_bash/03_transcodes/mov-cut-sec-rewrap.sh
@@ -3,9 +3,13 @@
 IFS=$'\n'
 
 if [[ -z "$1" ]]; then
-	echo "Please input the folder containing your video files"
+    echo "Please input the folder containing your video files"
 else
-	for f in "$1/"*.{mov,mkv,avi,mp4,wmv,webm,aiff,wav,aif}; do
-		ffmpeg -ss 00:01.000 -y -i "$f" -c:v copy -c:a copy ${f/./-min1.} ;
-	done
+    if [[ -f $1 ]]; then
+       	ffmpeg -ss 00:01.000 -y -i "$1" -c:v copy -c:a copy ${1/./-min1.} ;
+    elif [[ -d $1 ]]; then
+        for f in "$1/"*.{mov,mkv,avi,mp4,wmv,webm,aiff,wav,aif}; do
+            ffmpeg -ss 00:01.000 -y -i "$f" -c:v copy -c:a copy ${f/./-min1.} ;
+        done
+    fi
 fi
