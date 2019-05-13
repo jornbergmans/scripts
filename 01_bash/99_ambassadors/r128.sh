fa26cf38a31d5780c0dafdac3cf1db82cf9f2b95
JasonD <jason.dmail@gmail.com>
2019-05-13 10:20:51 +0200

    Finally fixed the looping on folders.

--- 01_bash/99_ambassadors/r128.sh
+++ 01_bash/99_ambassadors/r128.sh
@@ -10,3 +10,15 @@ elif [[ -d $1 ]]; then
     fi;
   done;
 fi
+
+
+function r128() { if [[ -f "$1" ]]; then
+                    filelist="$1"
+                  elif [[ -d "$1" ]]; then
+                    filelist=$(ls "$1"/*.{wav,mxf,aif,aiff,mov,mp4,mkv,avi,flv,webm})
+                  fi
+                  if [[ -f "$f" ]]; then
+                    IFS=$'\n' && for f in $filelist; do
+                      echo "$f" && ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:"
+                    done
+                  fi; }
