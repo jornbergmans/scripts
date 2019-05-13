d8b889300187e14273195fd1822d126c233e792d
JasonD <jason.dmail@gmail.com>
2019-05-13 10:35:21 +0200

    Using Find instead of ls to get a good list of files.

--- 01_bash/99_ambassadors/r128.sh
+++ 01_bash/99_ambassadors/r128.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
 if [[ -f $1 ]]; then
-  f="$1" ;
+  flist="$1" ;
 elif [[ -d $1 ]]; then
   flist=$(find "$1" -type f -iname "*.wav" -or -iname "*.mxf" -or -iname "*.mov" -or -iname "*.mp4" -or -iname "*.mkv" -or -iname "*.avi" -or -iname "*.flv" -or -iname "*.webm" -or -iname "*.aif*")
-  for f in $flist ; do
-    if [[ -f $f ]]; then
-      IFS=$'\n' basename "$f" && ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:";
-    fi;
-  done;
 fi
 
+for f in $flist ; do
+  if [[ -f $f ]]; then
+    IFS=$'\n' basename "$f" && ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:";
+  fi;
+done;
 
-function r128() { if [[ -f "$1" ]]; then
-                    filelist="$1"
-                  elif [[ -d "$1" ]]; then
-                    filelist=$(ls "$1"/*.{wav,mxf,aif,aiff,mov,mp4,mkv,avi,flv,webm})
-                  fi
-                  if [[ -f "$f" ]]; then
-                    IFS=$'\n' && for f in $filelist; do
-                      echo "$f" && ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:"
-                    done
-                  fi; }
+# if [[ -f "$1" ]]; then
+#   filelist="$1";
+# elif [[ -d "$1" ]]; then
+#   IFS='\n'
+#   filelist=$(ls "$1"/*.{wav,mxf,aif,aiff,mov,mp4,mkv,avi,flv,webm});
+# fi;
+#
+# for f in $filelist; do
+#   if [[ -f "$f" ]]; then
+#   echo "$f"
+#   ffprobe -f lavfi amovie="$f",ebur128=metadata=1 -show_frames 2>&1 | grep "I:";
+#   fi;
+# done
